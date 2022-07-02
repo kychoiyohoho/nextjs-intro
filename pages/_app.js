f7d645579ec8e4a485af0742645c650dd03c0fa5
kychoiyohoho <mjnhy2001@naver.com>
2022-07-02 12:49:25 +0900

    url 바꾸는거랑 이미지 띄우는거 Nextjs 기본

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Layout>
+        <Component {...pageProps} />
+        <footer>안녕하세요</footer>
+      </Layout>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
